638a7368a9b02414d8cb4a20a3da114c124c650e
mel10c <mel.zhu@mail.utoronto.ca>
2023-09-20 17:40:47 -0400

    config: website appearance

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,8 +7,10 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/mel10c",
+      "Instagram": "https://instagram.com/melaney_dxl?igshid=OGQ5ZDc2ODk2ZA==",
+      "小红书": "https://www.xiaohongshu.com/user/profile/5caae29b0000000011003637?xhsshare=CopyLink&appuid=5caae29b0000000011003637&apptime=1695227190",
+      "Resume": "https://github.com/mel10c/mel.zhu/blob/v4/content/documents/Resume.pdf",
     },
   }),
 }
